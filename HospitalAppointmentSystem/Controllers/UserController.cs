bd81259b1d5d31148ee2e7ca90008539c2e57a38
Esma Yıldız <eysilmadz@gmail.com>
2023-12-23 21:04:20 +0300

    User register added

--- HospitalAppointmentSystem/Controllers/UserController.cs
+++ HospitalAppointmentSystem/Controllers/UserController.cs
@@ -1,16 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HospitalAppointmentSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using HospitalAppointmentSystem.ViewModels;
 
 namespace HospitalAppointmentSystem.Controllers
 {
     public class UserController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult SignIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(RegisterViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser()
+                {
+                    IdentityNo = p.UserIdentityNo,
+                    UserName = p.UserName,
+                    Surname = p.UserSurname,
+                    Gender = p.UserGender,
+                    Birthday = p.UserBirthday,
+                    Email = p.UserMail,
+                };
+
+                var result = await _userManager.CreateAsync(user, p.UserPassword);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(p);
         }
     }
 }
---
+++ HospitalAppointmentSystem/Migrations/20231223143517_third.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalAppointmentSystem.Migrations
+{
+    public partial class third : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Birthday",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityNo",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityNo",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
