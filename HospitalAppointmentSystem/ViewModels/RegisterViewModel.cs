b38968118a665798281b968306c06b9d62cf9216
Esma Yıldız <eysilmadz@gmail.com>
2023-12-28 11:38:19 +0300

    RegisterViewModel added

---
+++ HospitalAppointmentSystem/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalAppointmentSystem.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string? UserIdentityNo { get; set; }
+
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        public string? UserSurname { get; set; }
+
+        [Required]
+        public string? UserGender { get; set; }
+
+        [Required]
+        public string? UserBirthday { get; set; }
+
+        [Required]
+        public string? UserMail { get; set; }
+
+        [Required]
+        public string? UserPassword { get; set; }
+
+        [Required]
+        [Compare("UserPassword",ErrorMessage ="Şifreler uyuşmuyor")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
